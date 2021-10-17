e35c720364335dbf89238dd4a5f90fd2523d188f
Gaurav Kulkarni <kulkarnigaurav38@gmail.com>
2021-10-17 15:42:00 +0530

    Create Electron App for hacktoberfest-issue-hunt

---
+++ src/views/scripts/functions.js
@@ -0,0 +1,36 @@
+const hacktoberfest = require("hacktoberfest-issue-hunt");
+hacktoberfest({ token: "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" })
+  .then((issues) => {
+    console.log(issues);
+
+    let index = 1;
+    // let inputLabelLength = document.getElementById("inputLabel").value.length;
+    // let inputLanguageLength = document.getElementById("inputLanguage").value.length;
+
+    // if () {
+    //   let label = document.getElementById("inputLabel").value;
+    //   let language = document.getElementById("inputLanguage").value;
+    //   let tableContent = issues.filter()
+    // } else {
+    let tableContent = issues.map((issue) => {
+      let patharray = issue.link.split("/");
+      let repoName = patharray[3];
+      return `<tr>
+                <td>${index++}</td>
+                <td>${repoName}</td>
+                <td>
+                  <a href="${issue.link}" target="_blank">${issue.title}</a>
+                </td>
+                <td>${issue.labels}</td>
+                <td>${issue.createdAt}</td>
+                <td>${issue.state}</td>
+                <td>${issue.comments}</td>
+
+              </tr>`;
+    });
+    // }
+    document
+      .querySelector("tbody")
+      .insertAdjacentHTML("afterbegin", tableContent);
+  })
+  .catch((err) => console.log(err));
